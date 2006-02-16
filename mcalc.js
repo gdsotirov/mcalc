d6011741857e6b5e075a0944fce2b6d6af4100b3
Georgi D. Sotirov <gdsotirov@dir.bg>
2006-02-16 19:13:27 +0000

    GPL-lization

--- mcalc.js
+++ mcalc.js
@@ -1,7 +1,21 @@
 /* Mortgage calculator Web Interface
- * ---
- * Written by George D. Sotirov (gdsotirov@dir.bg)
- * $Id: mcalc.js,v 1.7 2005/12/12 19:44:10 gsotirov Exp $
+ * Copyright (C) 2004-2006  Georgi D. Sotirov
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id: mcalc.js,v 1.8 2006/02/16 19:13:27 gsotirov Exp $
  */
 
 var uisPlsFillAmount = 0;
